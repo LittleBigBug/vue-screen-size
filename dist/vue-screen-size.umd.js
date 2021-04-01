6bd02a1ec82778d4268b55522926a190622bcb66
Ethan Jones <ethan@yasfu.net>
2021-03-31 20:43:42 -0600

    Maybe don't exclude dist files for modules lol

---
+++ dist/vue-screen-size.umd.js
@@ -0,0 +1,537 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('vue')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'vue'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.VueScreenSize = {}, global.Vue));
+}(this, (function (exports, Vue) { 'use strict';
+
+  function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+  var Vue__default = /*#__PURE__*/_interopDefaultLegacy(Vue);
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+  }
+
+  function _inherits(subClass, superClass) {
+    if (typeof superClass !== "function" && superClass !== null) {
+      throw new TypeError("Super expression must either be null or a function");
+    }
+
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+      constructor: {
+        value: subClass,
+        writable: true,
+        configurable: true
+      }
+    });
+    if (superClass) _setPrototypeOf(subClass, superClass);
+  }
+
+  function _getPrototypeOf(o) {
+    _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
+      return o.__proto__ || Object.getPrototypeOf(o);
+    };
+    return _getPrototypeOf(o);
+  }
+
+  function _setPrototypeOf(o, p) {
+    _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+      o.__proto__ = p;
+      return o;
+    };
+
+    return _setPrototypeOf(o, p);
+  }
+
+  function _isNativeReflectConstruct() {
+    if (typeof Reflect === "undefined" || !Reflect.construct) return false;
+    if (Reflect.construct.sham) return false;
+    if (typeof Proxy === "function") return true;
+
+    try {
+      Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {}));
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  function _assertThisInitialized(self) {
+    if (self === void 0) {
+      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
+
+    return self;
+  }
+
+  function _possibleConstructorReturn(self, call) {
+    if (call && (typeof call === "object" || typeof call === "function")) {
+      return call;
+    }
+
+    return _assertThisInitialized(self);
+  }
+
+  function _createSuper(Derived) {
+    var hasNativeReflectConstruct = _isNativeReflectConstruct();
+
+    return function _createSuperInternal() {
+      var Super = _getPrototypeOf(Derived),
+          result;
+
+      if (hasNativeReflectConstruct) {
+        var NewTarget = _getPrototypeOf(this).constructor;
+
+        result = Reflect.construct(Super, arguments, NewTarget);
+      } else {
+        result = Super.apply(this, arguments);
+      }
+
+      return _possibleConstructorReturn(this, result);
+    };
+  }
+
+  /**
+    * vue-class-component v7.2.6
+    * (c) 2015-present Evan You
+    * @license MIT
+    */
+
+  function _typeof(obj) {
+    if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
+      _typeof = function (obj) {
+        return typeof obj;
+      };
+    } else {
+      _typeof = function (obj) {
+        return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+      };
+    }
+
+    return _typeof(obj);
+  }
+
+  function _defineProperty(obj, key, value) {
+    if (key in obj) {
+      Object.defineProperty(obj, key, {
+        value: value,
+        enumerable: true,
+        configurable: true,
+        writable: true
+      });
+    } else {
+      obj[key] = value;
+    }
+
+    return obj;
+  }
+
+  function _toConsumableArray(arr) {
+    return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread();
+  }
+
+  function _arrayWithoutHoles(arr) {
+    if (Array.isArray(arr)) {
+      for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) arr2[i] = arr[i];
+
+      return arr2;
+    }
+  }
+
+  function _iterableToArray(iter) {
+    if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter);
+  }
+
+  function _nonIterableSpread() {
+    throw new TypeError("Invalid attempt to spread non-iterable instance");
+  } // The rational behind the verbose Reflect-feature check below is the fact that there are polyfills
+  // which add an implementation for Reflect.defineMetadata but not for Reflect.getOwnMetadataKeys.
+  // Without this check consumers will encounter hard to track down runtime errors.
+
+
+  function reflectionIsSupported() {
+    return typeof Reflect !== 'undefined' && Reflect.defineMetadata && Reflect.getOwnMetadataKeys;
+  }
+
+  function copyReflectionMetadata(to, from) {
+    forwardMetadata(to, from);
+    Object.getOwnPropertyNames(from.prototype).forEach(function (key) {
+      forwardMetadata(to.prototype, from.prototype, key);
+    });
+    Object.getOwnPropertyNames(from).forEach(function (key) {
+      forwardMetadata(to, from, key);
+    });
+  }
+
+  function forwardMetadata(to, from, propertyKey) {
+    var metaKeys = propertyKey ? Reflect.getOwnMetadataKeys(from, propertyKey) : Reflect.getOwnMetadataKeys(from);
+    metaKeys.forEach(function (metaKey) {
+      var metadata = propertyKey ? Reflect.getOwnMetadata(metaKey, from, propertyKey) : Reflect.getOwnMetadata(metaKey, from);
+
+      if (propertyKey) {
+        Reflect.defineMetadata(metaKey, metadata, to, propertyKey);
+      } else {
+        Reflect.defineMetadata(metaKey, metadata, to);
+      }
+    });
+  }
+
+  var fakeArray = {
+    __proto__: []
+  };
+  var hasProto = fakeArray instanceof Array;
+
+  function isPrimitive(value) {
+    var type = _typeof(value);
+
+    return value == null || type !== 'object' && type !== 'function';
+  }
+
+  function warn(message) {
+    if (typeof console !== 'undefined') {
+      console.warn('[vue-class-component] ' + message);
+    }
+  }
+
+  function collectDataFromConstructor(vm, Component) {
+    // override _init to prevent to init as Vue instance
+    var originalInit = Component.prototype._init;
+
+    Component.prototype._init = function () {
+      var _this = this; // proxy to actual vm
+
+
+      var keys = Object.getOwnPropertyNames(vm); // 2.2.0 compat (props are no longer exposed as self properties)
+
+      if (vm.$options.props) {
+        for (var key in vm.$options.props) {
+          if (!vm.hasOwnProperty(key)) {
+            keys.push(key);
+          }
+        }
+      }
+
+      keys.forEach(function (key) {
+        Object.defineProperty(_this, key, {
+          get: function get() {
+            return vm[key];
+          },
+          set: function set(value) {
+            vm[key] = value;
+          },
+          configurable: true
+        });
+      });
+    }; // should be acquired class property values
+
+
+    var data = new Component(); // restore original _init to avoid memory leak (#209)
+
+    Component.prototype._init = originalInit; // create plain data object
+
+    var plainData = {};
+    Object.keys(data).forEach(function (key) {
+      if (data[key] !== undefined) {
+        plainData[key] = data[key];
+      }
+    });
+
+    if (process.env.NODE_ENV !== 'production') {
+      if (!(Component.prototype instanceof Vue__default['default']) && Object.keys(plainData).length > 0) {
+        warn('Component class must inherit Vue or its descendant class ' + 'when class property is used.');
+      }
+    }
+
+    return plainData;
+  }
+
+  var $internalHooks = ['data', 'beforeCreate', 'created', 'beforeMount', 'mounted', 'beforeDestroy', 'destroyed', 'beforeUpdate', 'updated', 'activated', 'deactivated', 'render', 'errorCaptured', 'serverPrefetch' // 2.6
+  ];
+
+  function componentFactory(Component) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    options.name = options.name || Component._componentTag || Component.name; // prototype props.
+
+    var proto = Component.prototype;
+    Object.getOwnPropertyNames(proto).forEach(function (key) {
+      if (key === 'constructor') {
+        return;
+      } // hooks
+
+
+      if ($internalHooks.indexOf(key) > -1) {
+        options[key] = proto[key];
+        return;
+      }
+
+      var descriptor = Object.getOwnPropertyDescriptor(proto, key);
+
+      if (descriptor.value !== void 0) {
+        // methods
+        if (typeof descriptor.value === 'function') {
+          (options.methods || (options.methods = {}))[key] = descriptor.value;
+        } else {
+          // typescript decorated data
+          (options.mixins || (options.mixins = [])).push({
+            data: function data() {
+              return _defineProperty({}, key, descriptor.value);
+            }
+          });
+        }
+      } else if (descriptor.get || descriptor.set) {
+        // computed properties
+        (options.computed || (options.computed = {}))[key] = {
+          get: descriptor.get,
+          set: descriptor.set
+        };
+      }
+    });
+    (options.mixins || (options.mixins = [])).push({
+      data: function data() {
+        return collectDataFromConstructor(this, Component);
+      }
+    }); // decorate options
+
+    var decorators = Component.__decorators__;
+
+    if (decorators) {
+      decorators.forEach(function (fn) {
+        return fn(options);
+      });
+      delete Component.__decorators__;
+    } // find super
+
+
+    var superProto = Object.getPrototypeOf(Component.prototype);
+    var Super = superProto instanceof Vue__default['default'] ? superProto.constructor : Vue__default['default'];
+    var Extended = Super.extend(options);
+    forwardStaticMembers(Extended, Component, Super);
+
+    if (reflectionIsSupported()) {
+      copyReflectionMetadata(Extended, Component);
+    }
+
+    return Extended;
+  }
+
+  var reservedPropertyNames = [// Unique id
+  'cid', // Super Vue constructor
+  'super', // Component options that will be used by the component
+  'options', 'superOptions', 'extendOptions', 'sealedOptions', // Private assets
+  'component', 'directive', 'filter'];
+  var shouldIgnore = {
+    prototype: true,
+    arguments: true,
+    callee: true,
+    caller: true
+  };
+
+  function forwardStaticMembers(Extended, Original, Super) {
+    // We have to use getOwnPropertyNames since Babel registers methods as non-enumerable
+    Object.getOwnPropertyNames(Original).forEach(function (key) {
+      // Skip the properties that should not be overwritten
+      if (shouldIgnore[key]) {
+        return;
+      } // Some browsers does not allow reconfigure built-in properties
+
+
+      var extendedDescriptor = Object.getOwnPropertyDescriptor(Extended, key);
+
+      if (extendedDescriptor && !extendedDescriptor.configurable) {
+        return;
+      }
+
+      var descriptor = Object.getOwnPropertyDescriptor(Original, key); // If the user agent does not support `__proto__` or its family (IE <= 10),
+      // the sub class properties may be inherited properties from the super class in TypeScript.
+      // We need to exclude such properties to prevent to overwrite
+      // the component options object which stored on the extended constructor (See #192).
+      // If the value is a referenced value (object or function),
+      // we can check equality of them and exclude it if they have the same reference.
+      // If it is a primitive value, it will be forwarded for safety.
+
+      if (!hasProto) {
+        // Only `cid` is explicitly exluded from property forwarding
+        // because we cannot detect whether it is a inherited property or not
+        // on the no `__proto__` environment even though the property is reserved.
+        if (key === 'cid') {
+          return;
+        }
+
+        var superDescriptor = Object.getOwnPropertyDescriptor(Super, key);
+
+        if (!isPrimitive(descriptor.value) && superDescriptor && superDescriptor.value === descriptor.value) {
+          return;
+        }
+      } // Warn if the users manually declare reserved properties
+
+
+      if (process.env.NODE_ENV !== 'production' && reservedPropertyNames.indexOf(key) >= 0) {
+        warn("Static property name '".concat(key, "' declared on class '").concat(Original.name, "' ") + 'conflicts with reserved property name of Vue internal. ' + 'It may cause unexpected behavior of the component. Consider renaming the property.');
+      }
+
+      Object.defineProperty(Extended, key, descriptor);
+    });
+  }
+
+  function Component(options) {
+    if (typeof options === 'function') {
+      return componentFactory(options);
+    }
+
+    return function (Component) {
+      return componentFactory(Component, options);
+    };
+  }
+
+  Component.registerHooks = function registerHooks(keys) {
+    $internalHooks.push.apply($internalHooks, _toConsumableArray(keys));
+  };
+
+  undefined && undefined.__spreadArrays || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+
+    for (var r = Array(s), k = 0, i = 0; i < il; i++) for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++) r[k] = a[j];
+
+    return r;
+  }; // Code copied from Vue/src/shared/util.js
+
+  var _class;
+
+  var VCSInternalReactiveComponent = /*#__PURE__*/function (_Vue) {
+    _inherits(VCSInternalReactiveComponent, _Vue);
+
+    var _super = _createSuper(VCSInternalReactiveComponent);
+
+    function VCSInternalReactiveComponent() {
+      var _temp, _this;
+
+      _classCallCheck(this, VCSInternalReactiveComponent);
+
+      for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+      }
+
+      return _possibleConstructorReturn(_this, (_temp = _this = _super.call.apply(_super, [this].concat(args)), Object.defineProperty(_assertThisInitialized(_this), "event", {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        value: null
+      }), Object.defineProperty(_assertThisInitialized(_this), "vssWidth", {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        value: 0
+      }), Object.defineProperty(_assertThisInitialized(_this), "vssHeight", {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        value: 0
+      }), _temp));
+    }
+
+    return VCSInternalReactiveComponent;
+  }(Vue__default['default']);
+
+  var reactiveComponent = new VCSInternalReactiveComponent();
+
+  var VueScreenSizeMixin = Component(_class = /*#__PURE__*/function (_Vue2) {
+    _inherits(VueScreenSizeMixin, _Vue2);
+
+    var _super2 = _createSuper(VueScreenSizeMixin);
+
+    function VueScreenSizeMixin() {
+      _classCallCheck(this, VueScreenSizeMixin);
+
+      return _super2.apply(this, arguments);
+    }
+
+    _createClass(VueScreenSizeMixin, [{
+      key: "$vssEvent",
+      get: function get() {
+        return reactiveComponent.event;
+      }
+    }, {
+      key: "$vssWidth",
+      get: function get() {
+        return reactiveComponent.vssWidth;
+      }
+    }, {
+      key: "$vssHeight",
+      get: function get() {
+        return reactiveComponent.vssHeight;
+      }
+    }, {
+      key: "getScreenWidth",
+      value: function getScreenWidth() {
+        return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      }
+    }, {
+      key: "getScreenHeight",
+      value: function getScreenHeight() {
+        return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+      }
+    }, {
+      key: "handleResize",
+      value: function handleResize() {
+        var event = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+        reactiveComponent.event = event;
+        reactiveComponent.vssWidth = this.getScreenWidth();
+        reactiveComponent.vssHeight = this.getScreenHeight();
+      }
+    }, {
+      key: "$vssDestroyListener",
+      value: function $vssDestroyListener() {
+        window.removeEventListener('resize', this.handleResize);
+      }
+    }, {
+      key: "mounted",
+      value: function mounted() {
+        this.handleResize(); // Calculate now
+
+        window.addEventListener('resize', this.handleResize);
+      }
+    }, {
+      key: "destroyed",
+      value: function destroyed() {
+        this.$vssDestroyListener();
+      }
+    }]);
+
+    return VueScreenSizeMixin;
+  }(Vue__default['default'])) || _class;
+
+  var install = function install(Vue) {
+    Vue.mixin(VueScreenSizeMixin);
+  }; // Note that here we're not only exporting the install function, but
+  // also the mixin itself - this is so with can `Vue.use()` or
+  // `mixins: [],` it.
+
+
+  var index = {
+    install: install,
+    VueScreenSizeMixin: VueScreenSizeMixin
+  };
+
+  exports.default = index;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
